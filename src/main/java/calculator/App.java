daef425e132f57bda840fd1984c071c5fd004146
Yujin Lee <dbwls9040@gmail.com>
2024-07-25 11:13:29 +0900

    lever1 -1 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("123");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+
+
+
     }
 }
