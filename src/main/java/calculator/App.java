6ba19b89cc72df971f8563e99c8187479367dadf
Yujin Lee <dbwls9040@gmail.com>
2024-07-25 11:25:18 +0900

    lever1 -2 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,7 +12,11 @@ public class App {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치(index)를 받는다.
+        char operator = sc.next().charAt(0);
 
+        System.out.println("operator = " + operator);
 
 
     }
