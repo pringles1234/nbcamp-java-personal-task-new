90fa2034af0a8268d608763b366eaba0a8e8fb84
Yujin Lee <dbwls9040@gmail.com>
2024-07-25 12:05:59 +0900

    lever1 -3 나눗셈 오류

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,7 +16,27 @@ public class App {
         // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치(index)를 받는다.
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+            int result = 0;
+            if(operator == '+'){
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+            }
+            else if(operator == '-'){
+                result = num1 - num2;
+                System.out.println("결과: " + result);
+            }
+            else if(operator == '*'){
+                result = num1 * num2;
+                System.out.println("결과: " + result);
+            }
+            else if(operator == '/'){
+                result = num1 / num2;
+                System.out.println("결과: " + result);
+                    if(num2 == 0);{
+                    System.out.println("분모에 0이 입력될 수 없습니다.");
+                }
+
+            }
 
 
     }
