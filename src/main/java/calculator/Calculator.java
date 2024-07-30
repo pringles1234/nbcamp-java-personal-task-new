d2100a2b9ce236e96f1983fb1601dd9bfe883f37
Yujin Lee <dbwls9040@gmail.com>
2024-07-30 21:38:56 +0900

    lever2 -1 예외처리 제외 commit

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,28 @@
 package calculator;
 
+// 예외처리 아직 진도 못나가서 빼고 진행
+
+import java.util.ArrayList;
 
 public class Calculator {
-   // 주석
-    public void testMethod() {
 
+    ArrayList<Integer> resultSave = new ArrayList<Integer>(); //연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
+
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 == 0) { // 분모에 0이 입력될 수 없는 조건 달아줌
+                System.out.println("분모에 0이 입력될 수 없습니다.");
+            } else {
+                result = num1 / num2;
+            }
+        }
+        return result;
     }
 }
